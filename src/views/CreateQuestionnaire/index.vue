faca7f76b2011332fa58630a2533c83e75483274
laurenting <jent.k.lau@gmail.com>
2022-03-27 12:19:00 +0800

    init

---
+++ src/views/CreateQuestionnaire/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    create
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateQuestionnaire'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      mainProcessFile: 'src/main/index.js', // 主进程入口文件
+      rendererProcessFile: 'src/renderer/main.js', // 渲染进程入口文件
+      mainProcessWatch: ['src/main'] // 检测主进程文件在更改时将重新编译主进程并重新启动
+    }
+  }
+}
